cd7ef784c59dad55842ff719a1a3490e864c6a3f
adarshsharma584 <adarshsharma0862@gmail.com>
2025-11-02 19:52:09 +0530

    add the minor changes in the styling of the shorts page

--- Client/src/pages/Shorts.jsx
+++ Client/src/pages/Shorts.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
-
+import { BiLike } from "react-icons/bi";
+import { BiDislike } from "react-icons/bi";
+import { LiaCommentSolid } from "react-icons/lia";
+import { FaShare } from "react-icons/fa";
 function Shorts() {
   return (
-    <div className=" text-white font-bold bg-red-400 h-full w-full flex gap-2  items-start pt-4 px-4">
-      <div className="leftSide h-[85%] w-full bg-green-700  flex flex-col gap-1 justify-end py-12 px-8">
+    <div className=" text-white font-bold  h-full w-full flex gap-2  items-start pt-4 px-4">
+      {/* left */}
+      <div className="leftSide h-[85%] w-full   flex flex-col gap-1 justify-end py-12 px-8">
         <div className=" flex gap-4 items-center ">
           <img src="/" alt="" className="h-8 w-8 rounded-full border" />
-          <span className="text-md font-bold">username</span>
+          <span className="text-lg font-bold">username</span>
           <button className="bg-white text-gray-700 rounded-xl px-2 py-1 w-auto h-8">
             Subscribe
           </button>
         </div>
-        <div className ="text-xl font-bold ">video discription and music</div>
-        <div className ="text-lg font-semibold ">hashTags</div>
+        <div className="text-xl font-semibold  overflow-hidden text-ellipsis ">
+          video discription and music | Motivational Video  
+        </div>
+        <div className="text-lg font-semibold ">hashTags</div>
       </div>
-      <div className="h-[85%] min-w-[370px] bg-yellow-600 rounded-3xl">
+
+      {/* middle */}
+      <div className="h-[85%] min-w-[370px]  rounded-3xl">
         <video
           src=""
           className="w-full h-full rounded-3xl object-cover"
@@ -22,13 +30,24 @@ function Shorts() {
         ></video>
       </div>
 
-
       {/* right */}
-      <div className="rightSide h-[85%] w-full bg-blue-700 flex flex-col justify-start px-2 pt-10  items-start gap-4">
-        <button className = "h-12 w-12 rounded-full border flex items-center justify-center text-xl text-gray-800">l</button>
-        <button className = "h-12 w-12 rounded-full border flex items-center justify-center text-xl text-gray-800">d</button>
-        <button className = "h-12 w-12 rounded-full border flex items-center justify-center text-xl text-gray-800">c</button>
-        <button className = "h-12 w-12 rounded-full border flex items-center justify-center text-xl text-gray-800">s</button>
+      <div className="rightSide h-[85%] w-full  flex flex-col justify-start px-2 pt-10  items-start gap-2">
+        <button className="h-12 w-12 rounded-full border flex items-center justify-center text-2xl text-white">
+          <BiLike />
+        </button>
+        <span className="text-md ml-2">Like</span>
+        <button className="h-12 w-12 rounded-full border flex items-center justify-center text-2xl text-white">
+          <BiDislike />
+        </button>
+        <span className="text-md ml-2">Dislike</span>
+        <button className="h-12 w-12 rounded-full border flex items-center justify-center text-2xl text-white">
+          <LiaCommentSolid />
+        </button>
+        <span className="text-md ml-2">Comment</span>
+        <button className="h-12 w-12 rounded-full border flex items-center justify-center text-2xl text-white">
+          <FaShare />
+        </button>
+        <span className="text-md ml-2">Share</span>
       </div>
     </div>
   );
